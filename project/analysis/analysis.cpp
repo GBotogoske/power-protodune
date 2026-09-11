ccf7d923e3faa3c1b07e765a56a8ad1ac93f8094
David <d295944@dac.unicamp.br>
2026-09-11 15:00:40 -0300

    Adjust analysis logic for the new detector proportions

--- project/analysis/analysis.cpp
+++ project/analysis/analysis.cpp
@@ -8,15 +8,19 @@
 #include <vector>
 #include <TLegend.h>
 
-void analysis(std::string filename="../build/Data.root")
+void analysis(std::string filename="../build/Data_correct.root")
 {
     // Open file and get tree
     TFile *file = TFile::Open(filename.c_str());
     TTree *tree = (TTree*)file->Get("Primary_Hit");
 
-    // Create 2D histogram
-    TH2D *h2 = new TH2D("hvis","VIS(z,y)", 18, -4.5, 4.5, 16, -4, 4);
-    TH2D *h3 = new TH2D("huv","VUV(z,y)", 18, -4.5, 4.5, 16, -4, 4);
+    // Create 2D histogram for internal region
+    TH2D *hVisInt = new TH2D("hvis","VIS(z,y) - Internal", 18, -4.64103, 4.64103, 16, -4, 4);
+    TH2D *hUvInt = new TH2D("huv","VUV(z,y) - Internal",  18, -4.64103, 4.64103, 16, -4, 4);
+
+     // Create 2D histogram for external region
+    TH2D *hVisExt = new TH2D("hvis","VIS(z,y)  - External", 18, -4.272, 4.272, 16, -4, 4);
+    TH2D *hUvExt = new TH2D("huv","VUV(z,y) - External", 18, -4.272, 4.272, 16, -4, 4);
 
     // Fill the histogram
     Double_t z, y, f, f2;
@@ -27,50 +31,65 @@ void analysis(std::string filename="../build/Data.root")
 
     int number_files=1;
     float gain=40000.0/25000.0;
-    float ratio=5.0;
+    float ratio=1.0;
 
     Long64_t nentries = tree->GetEntries();
     for (Long64_t i=0; i<nentries; i++) 
     {
         tree->GetEntry(i);
 
-        if((z<(-1.547) || z>1.547))// && (y<(-3.5) || y<3.5) )
-        {
-            ratio=gain/number_files;
+        if((z>(-1.547) && z<1.547) && (y>(-3.5) && y<3.5))
+        {            
+            if(f>=1)
+            {
+                hVisInt->Fill(z, y,f*ratio);
+            }
+                
+            if(f2>=1) 
+            {
+                hUvInt->Fill(z,y,f2*ratio);
+            }
         }
         else
         {
-            ratio=1.0/number_files;
+            ratio = gain/number_files;
+            if(f>=1)
+            {
+                hVisExt->Fill(z, y,f*ratio);
+            }
+                
+            if(f2>=1) 
+            {
+                hUvExt->Fill(z,y,f2*ratio);
+            }
         }
-
-        if(f>=1)
-        {
-            h2->Fill(z, y,f*ratio);
-        }
-            
-        if(f2>=1) 
-        {
-            h3->Fill(z,y,f2*ratio);
-        } // fill with weight = f
     }
 
     // Draw heatmap
     TCanvas *c1 = new TCanvas();
-    h2->Draw("COLZ");  // COLZ = color map
-    c1->SaveAs("heatmapVIS.png");
+    hVisInt->Draw("COLZ");  // COLZ = color map
+    c1->SaveAs("heatmapVISInt.png");
 
     TCanvas *c2 = new TCanvas();
-    h3->Draw("COLZ");  // COLZ = color map
-    c2->SaveAs("heatmapUV.png");
+    hUvInt->Draw("COLZ");  // COLZ = color map
+    c2->SaveAs("heatmapUVInt.png");
+
+    TCanvas *c3 = new TCanvas();
+    hVisExt->Draw("COLZ");  // COLZ = color map
+    c3->SaveAs("heatmapVISExt.png");
+
+    TCanvas *c4 = new TCanvas();
+    hUvExt->Draw("COLZ");  // COLZ = color map
+    c4->SaveAs("heatmapUVExt.png");
 
    // --- Converter h2 para matriz ---
-    int nx = h2->GetNbinsX();
-    int ny = h2->GetNbinsY();
+    int nx = hVisExt->GetNbinsX();
+    int ny = hVisExt->GetNbinsY();
 
         // Criar histograma para o buffer
-    TH2D *h4 = new TH2D("hall","VUV+VIS(z,y)", nx, -4.5, 4.5, ny, -4, 4);
-    TH2D *hbuffer = new TH2D("hbuffer","UV/(UV+VIS)", nx, -4.5, 4.5, ny, -4, 4);
-    TH2D *hratio = new TH2D("hratio","UV/VIS", nx, -4.5, 4.5, ny, -4, 4);
+    TH2D *h4 = new TH2D("hall","VUV+VIS(z,y)", nx, -4.64103, 4.64103, ny, -4, 4);
+    TH2D *hbuffer = new TH2D("hbuffer","UV/(UV+VIS)", nx, -4.64103, 4.64103, ny, -4, 4);
+    TH2D *hratio = new TH2D("hratio","UV/VIS", nx, -4.64103, 4.64103, ny, -4, 4);
 
 
     std::vector<std::vector<double>> mat_uv(nx, std::vector<double>(ny,0));
@@ -88,18 +107,26 @@ void analysis(std::string filename="../build/Data.root")
     {
         for(int j=1; j<=ny; j++)
         {
-            mat_vis[i-1][j-1] = h2->GetBinContent(i,j);
-            mat_uv[i-1][j-1] = h3->GetBinContent(i,j);
+            if ((i > 6 && i < 13) && (j > 1 && j < ny)){
+                mat_vis[i-1][j-1] = hVisInt->GetBinContent(i,j);
+                mat_uv[i-1][j-1] = hUvInt->GetBinContent(i,j);
+            } else {
+                mat_vis[i-1][j-1] = hVisExt->GetBinContent(i,j);
+                mat_uv[i-1][j-1] = hUvExt->GetBinContent(i,j);
+            }
+            
             if(mat_vis[i-1][j-1]<cut_pe)
             {
                 mat_vis[i-1][j-1]=0;
             }
-             if(mat_uv[i-1][j-1]<cut_pe)
+            
+            if(mat_uv[i-1][j-1]<cut_pe)
             {
                 mat_uv[i-1][j-1]=0;
             }
+
             mat_all[i-1][j-1] = mat_uv[i-1][j-1] +  mat_vis[i-1][j-1];
-            buffer[i-1][j-1] = mat_uv[i-1][j-1]/(  mat_uv[i-1][j-1] + mat_vis[i-1][j-1] );
+            buffer[i-1][j-1] = mat_uv[i-1][j-1]/(mat_uv[i-1][j-1] + mat_vis[i-1][j-1] );
             myratio[i-1][j-1] = mat_uv[i-1][j-1]/(mat_vis[i-1][j-1] );
         }
     }
@@ -114,7 +141,6 @@ void analysis(std::string filename="../build/Data.root")
             h4->SetBinContent(i, j, mat_all[i-1][j-1]);
         }
     }
-
 
     int cont=0;
     for(int i=1; i<nx-1; i++)        
@@ -135,19 +161,19 @@ void analysis(std::string filename="../build/Data.root")
     std::cout << "MIN Light Yield: " << lightyield_min << std::endl;
 
     // Plotar
-    TCanvas *c3 = new TCanvas();
+    TCanvas *c5 = new TCanvas();
     hbuffer->GetZaxis()->SetRangeUser(0.0,1.0); // já que é uma fração
     hbuffer->Draw("COLZ");
-    c3->SaveAs("heatmapBuffer.png");
+    c5->SaveAs("heatmapBuffer.png");
 
-    TCanvas *c4 = new TCanvas();
+    TCanvas *c6 = new TCanvas();
     hratio->GetZaxis()->SetRangeUser(0.0,1.0); // já que é uma fração
     hratio->Draw("COLZ");
-    c4->SaveAs("heatmapRatio.png");
+    c6->SaveAs("heatmapRatio.png");
 
-    TCanvas *c5 = new TCanvas();
+    TCanvas *c7 = new TCanvas();
     h4->Draw("COLZ");  // COLZ = color map
-    c5->SaveAs("heatmapAll.png");
+    c7->SaveAs("heatmapAll.png");
 
     TH1D *h1d = new TH1D("hist_franc","Histogram Francesco", 100, 0 , 210 );
     for(int i=0; i<nx; i++)        
@@ -157,9 +183,9 @@ void analysis(std::string filename="../build/Data.root")
             h1d->Fill(mat_all[i][j]);
         }
     }
-    TCanvas *c6 = new TCanvas();
+    TCanvas *c8 = new TCanvas();
     h1d->Draw();  // COLZ = color map
-    c6->SaveAs("histFrancesco.png");
+    c8->SaveAs("histFrancesco.png");
 
    TH1D *h1dR = new TH1D("hist_R","R;R value;Entries", 200, -0.1 , 1.1 );
     for(int i=0; i<nx; i++)        
@@ -179,26 +205,24 @@ void analysis(std::string filename="../build/Data.root")
 
     for(int i=0; i<nx; i++)        
     {
-        for(int j=0; j<ny; j++)
+        for(int j=0; j<ny/2; j++)
         {
-            if(j<ny/2){
-                if(i>6 && i<(nx-6))
+            if(i>6 && i<(nx-6) && (j > 0) && (j < (ny-1))){
                 h1dRin->Fill(buffer[i][j]);
-                else {
-                    h1dRout->Fill(buffer[i][j]);
-                    if(buffer[i][j]>=out_cut)
-                    {
-                        out_cont_cut=out_cont_cut+1;
-                    }
-                    out_cont=out_cont+1;
-                }   
-            }    
+            } else {
+                h1dRout->Fill(buffer[i][j]);
+                if(buffer[i][j]>=out_cut)
+                {
+                    out_cont_cut=out_cont_cut+1;
+                }
+                out_cont=out_cont+1;
+            }
         }
     }
 
 
-    TCanvas *c7 = new TCanvas("c7","R distributions",800,600);
-    c7->SetGrid();
+    TCanvas *c9 = new TCanvas("c7","R distributions",800,600);
+    c9->SetGrid();
 
     h1dR->SetLineColor(kBlack);
     h1dR->SetLineWidth(2);
@@ -219,7 +243,7 @@ void analysis(std::string filename="../build/Data.root")
     leg->SetFillStyle(0);
     leg->Draw();
 
-    c7->SaveAs("histRATIO.png");
+    c9->SaveAs("histRATIO.png");
 
     std::cout << "efficiency: " << out_cont_cut/out_cont <<std::endl;
 
